349d681b745480655aaf1b02bea23446451c006e
marisela <mariselaalejandragomez@gmail.com>
2025-05-03 19:44:44 -0700

    Created virtual enviornment, .env file, and the databases created task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,25 @@
 from sqlalchemy.orm import Mapped, mapped_column
+# from sqlalchemy import Datetime
 from ..db import db
+
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    completed_at: Mapped[bool] = mapped_column(default=False, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed_at": bool(self.completed_at)
+        }
+
+    def from_dict(cls, task_data):
+        return cls(
+            title=task_data["title"],
+            description=task_data["description"],
+            completed_at=task_data["self.completed_at"]
+        )
